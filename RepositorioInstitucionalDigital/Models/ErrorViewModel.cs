320bf80dad9f9ac395b562be57f4ca71bcda7125
PatricioOsorio <PatricioMiguel_12@hotmail.com>
2022-10-22 13:03:34 -0500

    Add project files.

---
+++ RepositorioInstitucionalDigital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositorioInstitucionalDigital.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
