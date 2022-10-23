89507033a6518c882558567ce9974d269ec14072
PatricioOsorio <PatricioMiguel_12@hotmail.com>
2022-10-23 11:03:43 -0500

    Agregado los roles

--- RepositorioInstitucionalDigital/Controllers/HomeController.cs
+++ RepositorioInstitucionalDigital/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using RepositorioInstitucionalDigital.Models;
 using System.Diagnostics;
 
@@ -7,14 +9,37 @@ namespace RepositorioInstitucionalDigital.Controllers
   public class HomeController : Controller
   {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
     {
       _logger = logger;
+      _userManager = userManager;
+      _roleManager = roleManager;
     }
 
-    public IActionResult Index()
+    [AllowAnonymous]
+    public async Task<IActionResult> Index()
     {
+      var user = await _userManager.FindByEmailAsync("patriciomiguel_12@hotmail.com");
+
+      if (user == null)
+      {
+        await _roleManager.CreateAsync(new IdentityRole("ADMIN"));
+        await _roleManager.CreateAsync(new IdentityRole("SYSADMIN"));
+
+        var newUser = new IdentityUser()
+        {
+          UserName = "Patricio",
+          Email = "patriciomiguel_12@hotmail.com",
+          EmailConfirmed = true
+        };
+
+        await _userManager.CreateAsync(newUser, "Pato123.");
+        await _userManager.AddToRoleAsync(newUser, "SYSADMIN");
+
+      }
       return View();
     }
 
--- RepositorioInstitucionalDigital/Program.cs
+++ RepositorioInstitucionalDigital/Program.cs
@@ -12,8 +12,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>();
+    //.AddDefaultUI();
+
 builder.Services.AddControllersWithViews();
 
 // Configuracion de EmailSender
