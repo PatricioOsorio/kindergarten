1db60b421a5a899c97d6fc5e40423cd144860baa
PatricioOsorio <PatricioMiguel_12@hotmail.com>
2022-10-24 20:32:46 -0500

    Agregado inicializador de roles

--- RepositorioInstitucionalDigital/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RepositorioInstitucionalDigital/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -65,8 +65,8 @@ namespace RepositorioInstitucionalDigital.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+      //[EmailAddress]
+      [Display(Name = "Email o Username")]
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
